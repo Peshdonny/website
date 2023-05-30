e655f22ce82e7b46f98ff5f747e87edcfae3b0b3
Apesh <peshalungat@gmail.com>
2023-05-30 16:30:42 +0300

    updated blog

--- pesh/settings.py
+++ pesh/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pesh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
